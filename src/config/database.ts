9299adcb8715b82d0767681bcbabbcf04766a396
vijay <vijayshagara1221@gmail.com>
2026-01-26 19:33:22 +0530

    model added

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,11 @@
 import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
-import path from 'path';
 import HeatSchedule from '../models/heat-schedules.model';
+import Cow from '../models/cow.model';
+import HealthRecord from '../models/health-record.model';
+import HeatCycle from '../models/heat-cycle.model';
+import Task from '../models/task.model';
+import User from '../models/user.model';
 
 dotenv.config();
 
@@ -14,6 +18,11 @@ const sequelize = new Sequelize({
   dialect: 'postgres',
   models: [
     HeatSchedule,
+    Cow,
+    HealthRecord,
+    HeatCycle,
+    Task,
+    User,
   ],
   dialectOptions: {
     ssl: {
@@ -27,7 +36,6 @@ const sequelize = new Sequelize({
     acquire: 30000,
     idle: 10000,
   },
-  // models: [path.join(__dirname, '../models/*.model.ts')], // Explicit path
   logging: false,
 });
 
