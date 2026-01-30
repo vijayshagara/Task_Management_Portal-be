a5b5f62daa59f0e2f3202b3a01fb5a740c3f5edd
vijay <vijayshagara1221@gmail.com>
2026-01-30 16:20:31 +0530

    cors add for both mobile and destop

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,30 @@ import heatCycleRoutes from './routes/heat-cycle.routes';
 import taskRoutes from './routes/task.routes';
 
 const app = express();
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://toral-cattle-farm.netlify.app",
+];
 // --------------------
 // Middleware
 // --------------------
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin (Postman, mobile apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors());
 app.use(express.json({ limit: '10mb' })); // safety for payload size
 
 // --------------------
