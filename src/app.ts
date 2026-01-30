0ab29cedc6afff58d52c92a5d00add8825313aee
vijay <vijayshagara1221@gmail.com>
2026-01-30 16:27:18 +0530

    cors error  fixed

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.options("*", cors());
+app.options(/.*/, cors());
 app.use(express.json({ limit: '10mb' })); // safety for payload size
 
 // --------------------
