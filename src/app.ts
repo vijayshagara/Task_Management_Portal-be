8d95998374feac68d2ea25c6c71feb4ef2152982
vijay <vijayshagara1221@gmail.com>
2026-06-30 17:06:13 +0530

    add allowed origin for toral cattle farm

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://task-management-portal-be.vercel.app",
+  "https://toral-cattle-farm.netlify.app/"
   
 ];
 // --------------------
